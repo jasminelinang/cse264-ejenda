ced1570a005f57f73668794fa1f7f45d7b2508b8
jasminelinang <jcl326@lehigh.edu>
2025-11-22 09:12:09 -0500

    Fixed Supabase Database Connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,18 @@
+// server/db/index.js
 const postgres = require("postgres");
 require("dotenv").config();
 
-const sql = postgres(process.env.SUPABASE_DB_URL, {
+const sql = postgres({
+  host: "aws-0-us-west-2.pooler.supabase.com",
+  port: 5432,
+  database: "postgres",
+  username: "postgres.mbmvmdwxbbksczouzurq",
+  password: process.env.SUPABASE_DB_PASSWORD,
   ssl: "require",
 });
+
+console.log("Loaded DB password length:", process.env.SUPABASE_DB_PASSWORD?.length);
+console.log("DB password chars:", [...process.env.SUPABASE_DB_PASSWORD || ""]);
+
 
 module.exports = sql;
